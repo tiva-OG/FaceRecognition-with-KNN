cc157628adb401ef4d6fe1486277b493f585b555
Salvation Tiva <tivasalvation@gmail.com>
2024-01-21 21:12:05 +0100

    created a setup.py and a requirements file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_packages(file_path: str) -> List[str]:
+    """
+    this function returns a list of the required packages
+
+    :param file_path: path to file containing requirements
+    :return: list of requirements
+    """
+
+    with open(file_path) as requirements_file:
+        requirements = requirements_file.readlines()
+        requirements = [req.replace("/n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+if __name__ == '__main__':
+    setup(
+        name='GuessWho',
+        version='1.0.0',
+        author='tiva',
+        packages=find_packages(),
+        install_requires=get_packages('requirements.txt')
+    )
